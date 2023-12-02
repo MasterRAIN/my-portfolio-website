82addf6f22ec7a6eb5b4e3391c3a6e7e36df06f3
Rainier Barbacena <rainierbarbacena@gmail.com>
2023-12-02 22:07:32 +0800

    added blog page

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -3,28 +3,24 @@ import React, { useState } from 'react';
 const postsData = [
   {
     title: 'Unleashing the Power of PHP: A Comprehensive Guide to Writing Code with PHP',
-    author: 'Rainier',
     date: 'October 21, 2023',
     imageUrl: process.env.PUBLIC_URL + '/Images/php.jpg',
     content: 'PHP, a versatile and widely-used server-side scripting language, has been the backbone of dynamic web development for decades.',
   },
   {
     title: 'The Art and Science of Responsive Web Design',
-    author: 'Rainier',
     date: 'October 22, 2023',
     imageUrl: process.env.PUBLIC_URL + '/Images/responsive.jpg',
     content: 'In our ever-evolving digital landscape, the way we access information has undergone a profound transformation.',
   },
   {
     title: 'Crafting Dynamic User Interfaces: Writing Code with React',
-    author: 'Rainier',
     date: 'October 19, 2023',
     imageUrl: process.env.PUBLIC_URL + '/Images/react_code.jpg',
     content: 'React, a popular JavaScript library developed by Facebook, has revolutionized the way we build user interfaces for web applications.',
   },
   {
     title: 'Unveiling the Power of MySQL: A Comprehensive Guide to Database Management',
-    author: 'Rainier',
     date: 'October 20, 2023',
     imageUrl: process.env.PUBLIC_URL + '/Images/mysql.jpg',
     content: 'Databases are the backbone of modern software applications, enabling the storage, retrieval, and management of vast amounts of data.',
@@ -46,7 +42,7 @@ function Posts() {
   const randomPosts2 = postsData.slice(3, 7);
 
   return (
-    <div className="flex w-9/12 pt-10 pl-20 gap-10 pb-20">
+    <div className="flex w-8/12 pt-10 pl-20 gap-10 pb-20">
       <div className="w-7/12">
         {randomPosts.map((random, index) => (
           <a href="" key={index}>
@@ -55,10 +51,6 @@ function Posts() {
               <br />
               <h2 className="text-xl font-bold text-black dark:text-white post-title">{random.title}</h2>
               <h1 className="text-sm text-c font-bold flex gap-10">
-                <div className="flex align-center">
-                  <ion-icon name="person" class="h-4 w-4 mr-2"></ion-icon>
-                  {random.author}
-                </div>
                 <div className="flex align-center">
                   <ion-icon name="calendar" class="h-4 w-4 mr-2"></ion-icon>
                   {random.date}
@@ -78,7 +70,6 @@ function Posts() {
               <br />
               <h2 className="text-xl font-bold text-black dark:text-white post-title">{random2.title}</h2>
               <h1 className="text-sm text-c font-bold mb-4 flex gap-10">
-                <div className="flex align-center"><ion-icon name="person" class="h-4 w-4 mr-2"></ion-icon>{random2.author}</div>
                 <div className="flex align-center"><ion-icon name="calendar" class="h-4 w-4 mr-2"></ion-icon>{random2.date}</div>
               </h1>
               <p className="dark:text-a">{random2.content}</p>
@@ -113,7 +104,7 @@ function TrendingPosts() {
   };
 
   return (
-    <div className="w-4/12 h-fit sticky top-16 pt-10 pr-20">
+    <div className="w-4/12 h-fit fixed right-0 pt-10 pl-10 pr-20">
       <div className="rounded-md h-10 flex shadow-md overflow-hidden bg-white dark:text-a dark:bg-g transition-colors duration-300">
         <div className="w-16 grid place-content-center text-c">
           <ion-icon name="search"></ion-icon>
@@ -125,8 +116,8 @@ function TrendingPosts() {
           value={search}
           onChange={handleSearch}
         />
-        <button className="w-16 grid place-content-center text-error" onClick={handleClear}>
-          <ion-icon name="close"></ion-icon>
+        <button className="w-16 grid place-content-center text-error group" onClick={handleClear}>
+          <ion-icon name="close" class="p-1.5 rounded-md group-hover:bg-aa group-hover:dark:bg-g transition-colors duration-300"></ion-icon>
         </button>
       </div>
       <h2 className="text-xl font-bold dark:text-white p-4 overflow-auto">
@@ -142,10 +133,6 @@ function TrendingPosts() {
               <div className="w-full">
                 <h2 className="text-sm text-black font-bold post-title2">{trending.title}</h2>
                 <h1>
-                  <div className="flex align-center text-xs text-c">
-                    <ion-icon name="person" class="h-3 w-3 mr-2"></ion-icon>
-                    {trending.author}
-                  </div>
                   <div className="flex align-center text-xs text-c">
                     <ion-icon name="calendar" class="h-3 w-3 mr-2"></ion-icon>
                     {trending.date}
@@ -164,13 +151,8 @@ function TrendingPosts() {
 function Blog() {
   return (
     <div className="relative flex gap-10">
-      {/* <Posts />
-      <TrendingPosts /> */}
-      <div className="px-40 w-screen h-screen -mt-16 flex items-center justify-center">
-        <div className="w-fit text-3xl text-black dark:text-white">
-          <h1>In Progress . . .</h1>
-        </div>
-      </div>
+      <Posts />
+      <TrendingPosts />
     </div>
   );
 }
