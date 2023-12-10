56c9368dbbcfa0755b28b7aecf54e233459ed2f2
Rainier Barbacena <rainierbarbacena@gmail.com>
2023-12-10 19:27:31 +0800

    designed project tiles

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -32,7 +32,7 @@ function Projects() {
         <br />
         <div className="grid place-content-center">
           <Reveal distance={50}>
-            <div className="w-fit md:px-8 xs:px-2 py-2 flex md:gap-8 xs:gap-1 justify-center bg-white dark:bg-h rounded-lg backdrop-blur bg-opacity-25 dark:bg-opacity-25 transition-colors duration-300">
+            <div className="w-fit md:px-8 xs:px-2 py-2 flex md:gap-8 xs:gap-1 justify-center rounded-lg">
               {categories.map((category) => (
                 <button
                 key={category.id}
@@ -44,30 +44,28 @@ function Projects() {
             </div>
           </Reveal>
         </div>
-        <br />
         <div className="grid xl:grid-cols-3 lg:grid-cols-2 xs:grid-cols-1 gap-10 lg:py-10 md:px-44">
-          <div className="h-96 rounded-lg form-shadow backdrop-blur bg-opacity-25 dark:bg-opacity-25 overflow-hidden">
-            <div className="bg-oxford dark:bg-g flex gap-4 items-center text-aa px-6 py-3">
+          <div className="rounded-lg form-shadow group bg-oxford dark:bg-g text-aa overflow-hidden">
+            <div className="flex gap-4 items-center px-6 py-3">
               <ion-icon name="globe-outline" class="text-xl"></ion-icon>
               <h1 className="my-name">Personal Portfolio</h1>
             </div>
-          </div>
-          <div className="h-96 rounded-lg form-shadow backdrop-blur bg-opacity-25 dark:bg-opacity-25 overflow-hidden">
-            <div className="bg-oxford dark:bg-g flex gap-4 items-center text-aa px-6 py-3">
-              <ion-icon name="desktop-outline" class="text-xl"></ion-icon>
-              <h1 className="my-name">Smart EOD Utility</h1>
-            </div>
-          </div>
-          <div className="h-96 rounded-lg form-shadow backdrop-blur bg-opacity-25 dark:bg-opacity-25 overflow-hidden">
-            <div className="bg-oxford dark:bg-g flex gap-4 items-center text-aa px-6 py-3">
-              <ion-icon name="phone-portrait-outline" class="text-xl"></ion-icon>
-              <h1 className="my-name">Mobile Price Verifier</h1>
-            </div>
-          </div>
-          <div className="h-96 rounded-lg form-shadow backdrop-blur bg-opacity-25 dark:bg-opacity-25 overflow-hidden">
-            <div className="bg-oxford dark:bg-g flex gap-4 items-center text-aa px-6 py-3">
-              <ion-icon name="briefcase-outline"></ion-icon>
-              <h1 className="my-name">Personal Portfolio</h1>
+            <div className="relative">
+              <div className="absolute flex flex-col h-full w-1/2 bg-white text-h z-50 px-6 py-3 -translate-x-full group-hover:-translate-x-0 transition-all duration-300">
+                <h1 className="my-name text-sm">Tech Stack: </h1>
+                <ul className="text-xs list-disc px-4 flex-auto">
+                  <li>React JS</li>
+                  <li>Tailwind CSS</li>
+                  <li>Netlify</li>
+                </ul>
+                <div className="flex gap-2 text-c">
+                  <ion-icon name="link-outline"></ion-icon>
+                  <h1 className="text-xs border-b-2">
+                    <a href="https://rainiercondebarbacena.netlify.app/" target="_blank" rel="noopener noreferrer">Live</a>
+                  </h1>
+                </div>
+              </div>
+              <img src={process.env.PUBLIC_URL + "/Images/Projects/personal-portfolio.webp"} alt="" />
             </div>
           </div>
         </div>
